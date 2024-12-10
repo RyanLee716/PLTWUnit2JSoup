0a79baab65ea172ffd15be59e3f3938bb6acf282
Ryan Lee <lee2830@mydusd.org>
2024-12-10 15:43:34 -0800

    test to see if git is the issue

--- soup.java
+++ soup.java
@@ -6,7 +6,7 @@ import org.jsoup.nodes.*;
 class soup {
     public static void main(String args[]) {
         Document doc;
-        String url = "";
+        String url = "https://www.commonsensemedia.org/book-reviews/harry-potter-and-the-sorcerers-stone-harry-potter-book-1";
         try {
             doc = Jsoup.connect(url).get();
         } catch (IOException e) {
@@ -14,5 +14,7 @@ class soup {
             return;
         }
 
+        Elements parent_reviews = doc.getElementsByClass("user-reviews-reviews-list-full-adult");
+        System.out.println(parent_reviews);
     }
 }
